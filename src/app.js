05662b4d703e0a3cd29867d572564ee2fa7fc869
Hendersson Machin <henderssonconh@gmail.com>
2024-07-27 21:04:39 +0200

    Programada la ruta productos y productos/id y practica de query

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 // Importar http
 //No me funcionó "const http = require ("http")"
 
-import http from "http"
+/* import http from "http"
 
 const server = http.createServer( (request, response) => {
     //Función callback (solicitad/recibir)
-    console.log("Se realizo una peticion al servidor"); 
-
-    response.end("!"); 
+    //////////////////////////////////////
 
 })
 // Constante del Puerto
@@ -23,5 +21,72 @@ server.listen(PUERTO, () => {
 
 })
 
-// `` ALT + 96
+// `` ALT + 96 */
 
+//De nuevo no me sirvió el Conts required
+
+import express from "express"
+const app = express();
+const PUERTO = 8080; 
+
+app.get("/", (req, res) =>{
+
+    res.send("Sección Home")
+})
+
+
+
+app.listen(PUERTO, () =>{
+
+    console.log("Escuchando el puerto de Express")
+
+})
+
+const misProductos = [
+
+    {id: 1, nombre:"Fideos", precio: 2},
+    {id: 2, nombre:"Arroz", precio: 2},
+    {id: 3, nombre:"Pan", precio: 3},
+    {id: 4, nombre:"Leche", precio: 1},
+    {id: 5, nombre:"Queso", precio: 3},
+    {id: 6, nombre:"Mermelada", precio: 1},
+    {id: 7, nombre:"Vino", precio: 4},
+
+]
+
+app.get("/Productos", (req, res) => {
+    res.send({misProductos})
+
+});
+
+//Ruta por ID
+
+app.get("/Productos/:id", (req, res) => {
+    //Se envía un request a través de la propiedad Params
+    let {id} = req.params;
+
+    let productoBuscado = misProductos.find(producto => producto.id == id);
+
+    if (productoBuscado) {
+        res.send(productoBuscado)
+    } else {
+        res.send("Producto no encontrado, todo va a explotar.")
+    }
+})
+
+//Query se refiere a múltiples consultas a un endpoint, le tenemos que colocar "?" y el nombre de la consulta
+
+app.get("/clientes", (req, res) =>{
+
+/*  
+    let nombre = req.query.nombre;
+    let apellido = req.query.apellido; 
+                                        */
+
+    let {nombre, apellido} = req.query;
+
+    res.send(`Bienvenido ${nombre} ${apellido}`)
+
+
+
+})
