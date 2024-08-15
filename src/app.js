017b88576af640c5c7485f2399864ba1fb8c780c
Hendersson Machin <henderssonconh@gmail.com>
2024-08-15 17:23:31 +0200

    Se acomodó el Código para que se lea más limpio.

--- src/app.js
+++ src/app.js
@@ -17,16 +17,21 @@ server.listen(PUERTO, () => {
 
 
 const express = require("express"); 
+const app = express(); 
 const exphdbs = require ("express-handlebars");
 const cartRouter = require("./Managers/cart-Manager.js");
-const app = express(); 
 const PUERTO = 8080;
 const viewsRouter = require ("./routes/views.router.js")
 const arrayProductos = ("src/Data/Productos.json")
+const {engine} = require ("express-handlebars");
+
 
 //Reconoce la extensión:
 
 app.engine("handlebars", exphdbs.engine());
+
+//Express Handlebars
+app.engine("handlebars", engine());
 
 //Renderiza através de handlebars:
 
@@ -39,6 +44,7 @@ app.set("views", "./src/views");
 //Middleware: 
 
 app.use(express.json()); 
+app.use(express.static("./src/public"))
 
 
 //Le decimos al servidor que vamos a trabajar con JSON. 
@@ -66,10 +72,13 @@ app.get("/products/:pid", async (req, res) => {
 //Rutas
 
 app.use ("/", viewsRouter);
+app.get("/", (req, res) => {
+    res.send("Funcciona?")
+
+})
 //Listen
 
 app.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto http://localhost:${PUERTO}/`); 
 })
 
-app.use(express.static("./src/public"));
