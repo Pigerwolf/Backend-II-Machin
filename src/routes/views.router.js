6358b74d2295e637023f4cea7564836e217c2eec
Hendersson Machin <henderssonconh@gmail.com>
2024-08-16 04:49:28 +0200

    Agregada las vistas tienda y contacto (Vacías... Pero las agregué :D)

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -9,4 +9,15 @@ router.get("/", async (req, res) =>{
     res.render("index", {productos})
 })
 
+router.get("/contacto", (req, res) => {
+    res.render("contacto")
+})
+
+router.get("/tienda", async (req, res) => {
+    const productos = await manager.getProducts();
+
+    res.render("tienda", {productos})
+})
+
+
 module.exports = router;
